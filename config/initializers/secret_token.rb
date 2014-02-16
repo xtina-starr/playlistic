0135633ef3394a67f9f95655e98688e8cce8120a
Christina Thompson <c.thompsonms@gmail.com>
2014-02-16 14:17:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playlistic::Application.config.secret_key_base = 'e387826e407be50eb6adde288faa732577e40c0ae9beac23b603d3c696286950db6a289a0125125065e6774dc1cbe9085534bf95676b84418417292cdf97a215'

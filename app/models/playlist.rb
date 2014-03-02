58689914c5e4d574d23a3aa69b5ca3598acee0f2
Christina Thompson <c.thompsonms@gmail.com>
2014-03-02 08:33:28 -0800

    parsed response and created a song instance

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,9 +1,19 @@
-class Playlist <ActiveRecord::Base
+class Playlist < ActiveRecord::Base
 
   def self.get_playlist_by_similar_artist(artist)
-    # http://developer.echonest.com/api/v4/playlist/static?api_key=FILDTEOIK2HBORODV&artist=Weezer&type=artist-radio&results=5
-    response = HTTParty.get("http://developer.echonest.com/api/v4/playlist/static?api_key=#{ENV["ECHO_API_KEY"]}&artist=#{artist}&format=json&results=10&type=artist-radio")
-    pp response.parsed_response["response"]["songs"].map { |song| song["title"] }
+    response = HTTParty.get("http://developer.echonest.com/api/v4/playlist/static?api_key=#{ENV["ECHO_API_KEY"]}&artist=#{artist}&format=json&results=10&type=artist-radio&bucket=id:spotify-WW&bucket=tracks")
+    # pp response.parsed_response["response"]["songs"].map { |song| song["title"] }
+
+    # Song.new(response.parsed_response["response"]["songs"].last)
+
+    array_of_songs = response.parsed_response["response"]["songs"].map { |song| 
+      if song["tracks"].length > 0 
+        Song.new(song) 
+      end
+      }
+      array_of_songs.compact
+    # songs is an array
+    # {:response => { :songs => [{:id => "1"}, {:id => 2}, {:id => 3}] } }
   end
 
 end

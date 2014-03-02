9d2421efd6b924b699954f179051fcd61a4947fd
Christina Thompson <c.thompsonms@gmail.com>
2014-03-02 12:04:35 -0800

    creates playlist based on user input in view

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,11 +1,13 @@
 class PlaylistController < ApplicationController
 
   def player
-    @songs = Playlist.get_playlist_by_similar_artist("Beatles")
+    @songs = Playlist.get_playlist_by_similar_artist(params[:artist])
     @tracks = @songs.map do |track| 
      track.foreign_id.sub(/spotify-WW:track:/, "") 
     end
     @tracks = @tracks.join(",")
+
+    render "welcome/index"
   end
 
 end
